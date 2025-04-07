1f32a9b881e2c80fec0ca471e2e1fc6f94056eaf
Antoine Marteau <antoine.marteau@protonmail.com>
2025-04-07 16:33:08 +1000

    non-allocating and fast PLambdaBases evaluations

--- src/Polynomials/Polynomials.jl
+++ src/Polynomials/Polynomials.jl
@@ -139,6 +139,8 @@ export PCurlGradMonomialBasis
 
 include("PolynomialInterfaces.jl")
 
+include("Combinations.jl")
+
 include("CartProdPolyBases.jl")
 
 include("CompWiseTensorPolyBases.jl")
@@ -158,8 +160,6 @@ include("ChebyshevBases.jl")
 include("BernsteinBases.jl")
 
 include("ModalC0Bases.jl")
-
-include("Combinations.jl")
 
 include("PLambdaBases.jl")
 
