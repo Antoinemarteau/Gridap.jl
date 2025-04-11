956eea839e8518bc30a8f19fbd16b8adc90ad77d
Antoine Marteau <antoine.marteau@protonmail.com>
2025-04-11 18:34:19 +1000

    [broken] wip refactor indexing in PLambdaBases

--- src/Polynomials/Polynomials.jl
+++ src/Polynomials/Polynomials.jl
@@ -141,8 +141,6 @@ export PCurlGradMonomialBasis
 
 include("PolynomialInterfaces.jl")
 
-include("Combinations.jl")
-
 include("CartProdPolyBases.jl")
 
 include("CompWiseTensorPolyBases.jl")
@@ -162,6 +160,8 @@ include("ChebyshevBases.jl")
 include("BernsteinBases.jl")
 
 include("ModalC0Bases.jl")
+
+include("Combinations.jl")
 
 include("PLambdaBases.jl")
 
