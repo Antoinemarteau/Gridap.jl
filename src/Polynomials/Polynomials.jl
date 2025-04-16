65d45433a338868d1fd486ad266de469c1b92ae9
Antoine Marteau <antoine.marteau@protonmail.com>
2025-04-16 16:39:56 +1000

    cleaning and more test for PLambdaBases

--- src/Polynomials/Polynomials.jl
+++ src/Polynomials/Polynomials.jl
@@ -117,8 +117,13 @@ export ChebyshevBasis
 export BernsteinBasis
 export BernsteinBasisOnSimplex
 export FEECPolyBasis
-export PLambdaBasis
+
 export PmLambdaBasis
+export PmΛ_bubbles
+export PLambdaBasis
+export PΛ_bubbles
+export get_bubbles
+export print_indices
 
 export CompWiseTensorPolyBasis
 export QGradBasis
